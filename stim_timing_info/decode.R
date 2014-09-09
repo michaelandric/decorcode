003b928d96a2d733b81bdf3d2306a10b623cf7ab
michaelandric <andric.michael@gmail.com>
2014-09-09 12:22:02 +0200

    adding dir and code for doing stimulus times

---
+++ stim_timing_info/decode.R
@@ -0,0 +1,49 @@
+# function that creates 1D stimulus files from the func run timings
+"This does in seconds"
+decoder <- function(x)
+{
+    stimvec = rep(0, 480)
+    begends = c()
+    begin = 11
+    for (mm in (x))
+    {   
+        end = begin+mm
+        begends = c(begends, begin, end)
+        stimvec[c(begin:end)] = 1
+        begin = end+40
+    }
+    #begends = matrix(begends, ncol = 2, byrow = 2)
+    list(stimuli_vec = stimvec, startfinish = begends)
+}
+stim1d = decoder(movs_in_secs[c(7,8,10)])
+
+
+
+AV1 = c(7, 8, 10)
+AV2 = c(4, 9, 16)
+AV3 = c(15, 6)
+SC1 = c(8, 16, 7)
+SC2 = c(15, 4, 10)
+SC3 = c(9, 6)
+SC4 = c(7, 16, 8)
+SC5 = c(4, 10, 15)
+SC6 = c(6, 9)
+"
+decoder <- function(x)
+{
+    stimvec = rep(0, 320)
+    begends = c()
+    begin = 11/1.5
+    for (mm in (x/1.5))
+    {   
+        end = begin+mm
+        begends = c(begends, begin, end)
+        stimvec[c(begin:end)] = 1
+        begin = end+(40/1.5)
+    }
+    begends = matrix(begends, ncol = 2, byrow = 2)
+    list(stimuli_vec = stimvec, startfinish = begends)
+}
+
+stimulus1d = decoder(movs_in_secs[c(7,8,10)])
+"
---
+++ stim_timing_info/gettimes.R
@@ -0,0 +1,35 @@
+#little code to get the timing info summarized
+rr = read.table("AR_timings")
+numsegments <- length(rr$V1)
+
+secondsAR <- c()
+minsAR <- c()
+
+for (i in 1:numsegments)
+{
+    secondsAR <- c(secondsAR, as.numeric(noquote(strsplit(noquote(levels(factor(rr$V3[i]))),":")[[1]]))[2])
+    minsAR <- c(minsAR, as.numeric(noquote(strsplit(noquote(levels(factor(rr$V3[i]))),":")[[1]]))[1])
+}
+
+print(sum(secondsAR) / 60 + (sum(minsAR)))
+ARtimes = (minsAR * 60) + secondsAR
+
+rr = read.table("AWH_timings")
+numsegments <- length(rr$V1)
+secondsAF <- c()
+minsAF <- c()
+
+for (i in 1:numsegments)
+{
+    secondsAF <- c(secondsAF, as.numeric(noquote(strsplit(noquote(levels(factor(rr$V3[i]))),":")[[1]]))[2])
+    minsAF <- c(minsAF, as.numeric(noquote(strsplit(noquote(levels(factor(rr$V3[i]))),":")[[1]]))[1])
+}
+
+print(sum(secondsAF) / 60 + (sum(minsAF)))
+AFtimes = (minsAF * 60) + secondsAF
+
+
+movs_in_secs = c(AFtimes[2], ARtimes[2], ARtimes[11], ARtimes[7], AFtimes[12], ARtimes[8], AFtimes[5], AFtimes[1], AFtimes[7], AFtimes[3], ARtimes[10], AFtimes[6], AFtimes[4], ARtimes[1], AFtimes[13], ARtimes[5])
+movs_in_secs[7] = 116   # adjustment for this movie too short estmate
+movs_in_secs_final = movs_in_secs[c(4,9,16,7,8,10,6,15)]
+

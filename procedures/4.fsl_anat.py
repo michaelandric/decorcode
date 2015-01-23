db0542c96fb38cc918da0e46ada39e437c573375
Michael Andric <andric.michael@gmail.com>
2015-01-23 17:39:36 +0100

    update fsl_anat

--- procedures/4.fsl_anat.py
+++ procedures/4.fsl_anat.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python
 
 import os
-import shutil
 from shlex import split
 from subprocess import call
 from subprocess import Popen
-from subprocess import PIPE
 from subprocess import STDOUT
 
+class FSLANAT:
 
-def _AFNItoNIFTI(t1):
-    f = open('stdout_files/stdout_from_afnitonifti_%(t1)s' % locals(), 'w')
-    cmdargs = split('3dAFNItoNIFTI %(t1)s.gert_reco+orig.' % locals())
-    call(cmdargs, stdout = f, stderr = STDOUT)
-    f.close()
+    def __init__(self, anat):
+        print 'Initializing... \nRequires anat'
+        self.t1pref = anat   # this is the T1 volume prefix
+        print 'Got it.'
 
-def _fslanat(t1):
-    f = open('stdout_files/stdout_from_fsl_anat_%(t1)s' % locals(), 'w')
-    fslargs = split('fsl_anat -i %(t1)s.gert_reco.nii.gz' % locals())    
-    Popen(fslargs, stdout = f, stderr = STDOUT)
-    f.close()
+    def AFNItoNIFTI(self):
+        print 'AFNItoNIFTI...'
+        f = open('stdout_files/stdout_from_afnitonifti_%s' % self.t1pref, 'w')
+        cmdargs = split('3dAFNItoNIFTI %s+orig.' % self.t1pref)
+        call(cmdargs, stdout = f, stderr = STDOUT)
+        f.close()
+        print 'DONE...'
+
+    def fslanat(self):
+        print 'fslanat...'
+        f = open('stdout_files/stdout_from_fsl_anat_%s' % self.t1pref, 'w')
+        fslargs = split('fsl_anat -i %s.nii.gz' % self.t1pref)
+        Popen(fslargs, stdout = f, stderr = STDOUT)
+        f.close()
+        print 'DONE'
 
 
-
-subj_list = ['JNWL']
-t1_list = ['mprage1', 'mprage2', 'mprage_2ndsess']
+subj_list = ['CRSA']
+t1_list = ['mprage1']   # localizer only the one brain
 
 if __name__ == "__main__":
-
     for ss in subj_list:
-        os.chdir(os.environ['decor']+'/%(ss)s' % locals())
+        print 'LOCALIZERS!'
+        os.chdir(os.environ['decor']+'/localizers/%(ss)s' % locals())   # set for localizers
         for mprage in t1_list:
-            t1 = '%(ss)s.%(mprage)s' % locals()
-            _AFNItoNIFTI(t1)
-            _fslanat(t1)
+            anat = '%(ss)s.%(mprage)s.gert_reco' % locals()
+            FA = FSLANAT()
+            FA.AFNItoNIFTI()
+            FA.fslanat()

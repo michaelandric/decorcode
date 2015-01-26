72039432f4716b0c15f47af1145772cfdf63e1f5
Michael Andric <andric.michael@gmail.com>
2015-01-26 17:17:36 +0100

    for localizer test data

--- procedures/6.align_vol.py
+++ procedures/6.align_vol.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python
 
 import os
-import shutil
 from shlex import split
 from subprocess import call
-from subprocess import Popen
-from subprocess import PIPE
 from subprocess import STDOUT
 
 
 def align_epis(ss, t1):
     f = open('stdout_files/stdout_from_align_vol.txt', 'w')
     #cmdargs = split('align_epi_anat.py -anat %(ss)s.%(t1)s.gert_reco.anat/T1_biascorr_brain.nii.gz -anat_has_skull no \
-    #cmdargs = split('align_epi_anat.py -anat T1_biascorr_brain.nii.gz -anat_has_skull no \
     #                -epi %(ss)s_sess1_meanepi+orig -epi_base 0 -suffix _%(t1)s -anat2epi -big_move' % locals())
+    '''below cmd is for localizer test'''
     cmdargs = split('align_epi_anat.py -anat %(ss)s.%(t1)s.gert_reco.anat/T1_biascorr_brain.nii.gz -anat_has_skull no \
-                    -epi %(ss)s_sess1_meanepi+orig -epi_base 0 -suffix _%(t1)s -anat2epi -big_move' % locals())
+                    -epi errts.%(ss)s.localizer.6mmblur_REML+orig -epi_base 0 -suffix _%(t1)s -anat2epi -big_move' % locals())
     call(cmdargs, stdout = f, stderr = STDOUT)
     f.close()
 
 
-subj_list = ['LSRS']
-t1_list = ['mprage1', 'mprage2', 'mprage_2ndsess']
-#t1_list = ['mprage2', 'mprage_2ndsess']
+subj_list = ['CRSA']
+#t1_list = ['mprage1', 'mprage2', 'mprage_2ndsess']
+t1_list = ['mprage1']
 
 if __name__ == "__main__":
     for ss in subj_list:
         for t1 in t1_list:
-            os.chdir(os.environ['decor']+'/%(ss)s' % locals())
+            os.chdir(os.environ['decor']+'/localizers/%s' % ss)   # adjusted for localizer
             align_epis(ss, t1)
 

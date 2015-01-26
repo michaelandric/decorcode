2d1edd52b1a9a52754d6fee6595a068aa0800312
Michael Andric <andric.michael@gmail.com>
2015-01-26 17:46:24 +0100

    make mean to align to for localizer

---
+++ procedures/5.align_epis_localizer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+'''
+This is necessary to make a mean in order to get better alignment to volume
+'''
+
+import os
+from shlex import split
+from subprocess import call
+from subprocess import STDOUT
+
+def make_mean(inpref):
+    f = open('stdout_files/stdout_from_align_vol.txt', 'w')
+    cmdargs = split('3dTstat -mean -prefix mean.%s %s+orig' % (inpref, inpref))
+    call(cmdargs, stdout = f, stderr = STDOUT)
+    f.close()
+
+
+subj_list = ['CRSA']
+
+if __name__ == "__main__":
+    for ss in subj_list:
+        os.chdir(os.environ['decor']+'/localizers/%s' % ss)   # adjusted for localizer
+        inpref = 'errts.%s.localizer.6mmblur_REML' % ss
+        make_mean(inpref)

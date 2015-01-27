230771a139cbc412ed993c5e3b39a2ab8cbd6ab4
Michael Andric <andric.michael@gmail.com>
2015-01-27 13:25:36 +0100

    tim_times_subtract option
    because I splice first 21 sec of the run, use this option so timing
    files are correct

--- procedures/16.localizer_deconvolve.py
+++ procedures/16.localizer_deconvolve.py
@@ -22,11 +22,15 @@ def deconvolve(ss):
 
 def testdecon(bl, fname):
     f = open('test_decon_stdout.txt', 'w')
+    '''
+    Includes removal of 21 sec (what was splaced at beginning of run to stabilize
+    '''
     cmdargs = split("3dDeconvolve -nodata -polort -1 -num_stimts 4 \
                     -stim_times 1 onlyA.%(ss)s.txt '%(bl)s(21)' \
                     -stim_times 2 onlyV.%(ss)s.txt '%(bl)s(21)' \
                     -stim_times 3 AATTN.%(ss)s.txt '%(bl)s(21)' \
                     -stim_times 4 VATTN.%(ss)s.txt '%(bl)s(21)' \
+                    -stim_times_subtract 21 \
                     -x1D stdout: \
                     | 1dplot -png %(fname)s -stdin -one -thick \
                     -xlabel TIME -ynames onlyA onlyV AATTN VATTN" % locals())

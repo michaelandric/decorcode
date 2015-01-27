f7aef910a78501c9da6b6efa3518550d3bf454ac
Michael Andric <andric.michael@gmail.com>
2015-01-27 13:19:55 +0100

    work on decon for localizer

---
+++ procedures/16.localizer_deconvolve.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+'''
+Seeing if the localizer works
+'''
+
+from shlex import split
+
+def deconvolve(ss):
+    f = open('stdout_files/stdout_from_deconvolve.txt', 'w')
+    cmdargs = split('3dDeconvolve -input errts.%(ss)s.localizer.6mmblur_REML+orig \
+                    -censor %(ss)s.localizer.6mmblur.results/censor_%(ss)s.localizer.6mmblur_combined_2.1D \
+                    -polort A -num_stimts 4 \
+                    -stim_times 1 onlyA.%(ss)s.txt MIONN(21) \
+                    -stim_times 2 onlyV.%(ss)s.txt MIONN(21) \
+                    -stim_times 3 AATTN.%(ss)s.txt MIONN(21) \
+                    -stim_times 4 VATTN.%(ss)s.txt MIONN(21) \
+                    -fout -tout -x1D X.xmat.%(ss)s.1D \
+                    -errts decon.err.%(ss)s \
+                    -bucket decon.stats.%(ss)s ' % locals())
+    call(cmdargs, stdout = f, stderr = STDOUT)
+    f.close()
+
+def testdecon(bl, fname):
+    f = open('test_decon_stdout.txt', 'w')
+    cmdargs = split("3dDeconvolve -nodata -polort -1 -num_stimts 4 \
+                    -stim_times 1 onlyA.%(ss)s.txt '%(bl)s(21)' \
+                    -stim_times 2 onlyV.%(ss)s.txt '%(bl)s(21)' \
+                    -stim_times 3 AATTN.%(ss)s.txt '%(bl)s(21)' \
+                    -stim_times 4 VATTN.%(ss)s.txt '%(bl)s(21)' \
+                    -x1D stdout: \
+                    | 1dplot -png %(fname)s -stdin -one -thick \
+                    -xlabel TIME -ynames onlyA onlyV AATTN VATTN" % locals())
+    call(cmdargs, stdout = f, stderr = STDOUT)
+    f.close()
+
+subj_list = ['CRSA']
+
+if __name__ == "__main__":
+    for ss in subj_list:
+        os.chdir(os.environ['decor']+'/localizers/%s' % ss)   # adjusted for localizer
+        #deconvolve(ss)
+        for bltype in ['MIONN', 'WAV']:
+            fname = 'test.%s' % bltype
+            testdecon(bltype, fname)

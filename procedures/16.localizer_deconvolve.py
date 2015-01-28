19e4722492b2885ffd2866d994650050f75e177c
Michael Andric <andric.michael@gmail.com>
2015-01-28 12:29:02 +0100

    trying BLOCK model

--- procedures/16.localizer_deconvolve.py
+++ procedures/16.localizer_deconvolve.py
@@ -15,10 +15,10 @@ def deconvolve(ss, model):
     '''
     cmdargs = split("3dDeconvolve -input errts.%(ss)s.localizer.6mmblur_REML+orig \
                     -polort A -num_stimts 4 \
-                    -stim_times 1 stim_timing/onlyA.%(ss)s.txt %(model)s(21) -stim_label 1 onlyA \
-                    -stim_times 2 stim_timing/onlyV.%(ss)s.txt %(model)s(21) -stim_label 2 onlyV \
-                    -stim_times 3 stim_timing/AATTN.%(ss)s.txt %(model)s(21) -stim_label 3 AATTN \
-                    -stim_times 4 stim_timing/VATTN.%(ss)s.txt %(model)s(21) -stim_label 4 VATTN \
+                    -stim_times 1 stim_timing/onlyA.%(ss)s.txt %(model)s(21,1) -stim_label 1 onlyA \
+                    -stim_times 2 stim_timing/onlyV.%(ss)s.txt %(model)s(21,1) -stim_label 2 onlyV \
+                    -stim_times 3 stim_timing/AATTN.%(ss)s.txt %(model)s(21,1) -stim_label 3 AATTN \
+                    -stim_times 4 stim_timing/VATTN.%(ss)s.txt %(model)s(21,1) -stim_label 4 VATTN \
                     -stim_times_subtract 21 \
                     -gltsym 'SYM: onlyA -onlyV -AATTN -VATTN' -glt_label 1 onlyAcontr \
                     -gltsym 'SYM: -onlyA onlyV -AATTN -VATTN' -glt_label 2 onlyVcontr \
@@ -58,7 +58,7 @@ subj_list = ['IAGO']
 if __name__ == "__main__":
     for ss in subj_list:
         os.chdir(os.environ['decor']+'/localizers/%s' % ss)   # adjusted for localizer
-        for mm in ['MION', 'MIONN']:
+        for mm in ['BLOCK']:
             deconvolve(ss, mm)
         #for bltype in ['MION']:
         #    testdecon(ss, bltype)

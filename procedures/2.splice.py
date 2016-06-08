4802ed60b13ed756e2d0b73ea3464c18ab2b0ab3
michaelandric <andric.michael@gmail.com>
2016-06-08 15:21:38 +0200

    modified 2.splice.py with setlog.py and new subjects

--- procedures/2.splice.py
+++ procedures/2.splice.py
@@ -1,33 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May  5 18:26:53 2016
+Created on Thu May  5 18:26:53 2016.
 
+Re-did logging with setlog.py June 8 2016
 @author: andric
 """
 
 import os
 from shlex import split
+from setlog import setup_log
 from subprocess import Popen
 from subprocess import STDOUT
+from subprocess import PIPE
 
 
-def splicer(subj, run):
-    stdf = open('stdout_files/stdout_from_Tcat_{}.txt'.format(run), 'w')
+def splicer(log, subj, run):
+    """Splice time series."""
+    log.info('Doing splicer %s %s ...', (subj, run))
     cmdargs = split('3dTcat -prefix {}.{}.TRIM \
                     raw.{}.{}.gert_reco+orig.BRIK[5-324]'.format(
                         run, subj, subj, run))
-    Popen(cmdargs, stdout=stdf, stderr=STDOUT)
-    stdf.close()
+    log.info('cmd: \n%s', cmdargs)
+    proc = Popen(cmdargs, stdout=PIPE, stderr=STDOUT)
+    log.info(proc.stdout.read())
 
-def splicer_rest(subj, run):
-    stdf = open('stdout_files/stdout_from_Tcat_{}.txt'.format(run), 'w')
+
+def splicer_rest(log, subj, run):
+    """Splice rest time series."""
+    log.info('Doing splicer_rest')
     cmdargs = split('3dTcat -prefix {}.{}.TRIM \
                     raw.{}.{}.gert_reco+orig.BRIK[5-164]'.format(
                         run, subj, subj, run))
-    Popen(cmdargs, stdout=stdf, stderr=STDOUT)
-    stdf.close()
+    proc = Popen(cmdargs, stdout=PIPE, stderr=STDOUT)
+    log.info(proc.stdout.read())
+
 
 def splicer_localizer(subj, run):
+    """Splice localizer series."""
     stdf = open('stdout_files/stdout_from_Tcat_{}.txt'.format(run), 'w')
     cmdargs = split('3dTcat -prefix {}.{}.TRIM \
                     raw.{}.{}.gert_reco+orig.BRIK[14-405]'.format(
@@ -41,10 +50,13 @@ if __name__ == "__main__":
     SCRUNS = ['SC{}'.format(i) for i in range(1, 7)]
     AVRUNS = ['AV1.1', 'AV1.2', 'AV2.1', 'AV2.2', 'AV3.1', 'AV3.2']
     RUNIDS = SCRUNS + AVRUNS
-    SUBJECTLIST = ['LNSE', 'PMBI']
+    SUBJECTLIST = ['LNDR', 'GOPR', 'ANRC', 'DAHL']
 
     for ss in SUBJECTLIST:
-        os.chdir(os.path.join(os.environ['decor'], ss))
+        subjectdir = os.path.join(os.environ['decor'], ss)
+        os.chdir(subjectdir)
+        logfile = setup_log(os.path.join(subjectdir, 'splicer'))
+        logfile.info('started splicer.py')
         for rr in RUNIDS:
-            splicer(ss, rr)
-        splicer_rest(ss, 'Rest')
+            splicer(logfile, ss, rr)
+        splicer_rest(logfile, ss, 'Rest')
